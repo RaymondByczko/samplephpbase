ef1c308a1093dca46d8057f56f9b1ab151ac7b05
Raymond Byczko <raymondbyczko@att.net>
2012-02-23 19:55:33 -0500

    RByczko self - samplephp

---
+++ public_html/samplephp/index.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<title>SamplePHP - Home</title>
+<link rel=stylesheet type="text/css"
+	href="http://www.lunarrays.com/samplephp/styles/gen_styles.css"
+	title="SamplePHPStyles">
+<script language="JavaScript" type="text/javascript" src="javascript/generalevents.js"></script>
+</head>
+<body>
+<div id="index_html">
+<H1>Home for samplePHP by Ray Byczko</H1>
+<table rules=none>
+<tr>
+<td class="leftc"><a href="ajax.php" onMouseOver="mouseOverDisplayImg('ajaxid')" >Ajax samples</a></td>
+<td>some data</td>
+<td id="output">more data</td>
+<td>yet more</td>
+</tr>
+
+<tr>
+<td class="leftc"><a href="gimp.php" onMouseOver="mouseOverDisplayImg('gimpid')" >Gimp Samples</td>
+<td>etc</td>
+<td>etc2</td>
+<td>etc3</td>
+</tr>
+
+<tr>
+<td class="leftc"><a href="organicchem.php" onMouseOver="mouseOverDisplayImg('organicchemid')" >Organic Chemistry</td>
+<td>etc</td>
+<td>etc2</td>
+<td>etc3</td>
+</tr>
+
+
+</table>
+</div>
+</body>
+</html>
+
